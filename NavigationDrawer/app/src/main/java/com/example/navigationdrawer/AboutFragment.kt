d26f7ef838708294c02f4ca87aa63fcf5d8feb8f
Akshatb2006 <kysuakshat23@gmail.com>
2025-04-17 10:31:59 +0530

    new

---
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/AboutFragment.kt
@@ -0,0 +1,17 @@
+package com.example.navigationdrawer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AboutFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+    }
+
---
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/ShareFragment.kt
@@ -0,0 +1,15 @@
+package com.example.navigationdrawer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ShareFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+    }
